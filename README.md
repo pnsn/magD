2b6ed2c97b3e62a07538f329a1d64a2d8b7aa513
Jon Connolly <jontconnolly@gmail.com>
2019-04-26 11:59:38 -0700

    added licence and setup.py thingies

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ the Brune model, written by Dan McNamara in C and ported to python
 
 magD has been tested on python 3.5 and greater
 
+MagD allows an end user to evaluate seismograph performance at the network level.
+
 ## Client
 
 An example client repo is at https://github.com/pnsn/magd_client refer to this repo for details
--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name='magD',
-    version='0.9.0',
-    packages=['magD'],
-    author='Jon Connolly',
-    author_email='joncon@uw.edu',
-    url='https://github.com/pnsn/magD',
-    python_requires='~-3.5.2',
-    dependency_links=[
-        'https://github.com/matplotlib/basemap/archive/v1.1.0.tar.gz'],
-    install_requires=[
-        'numpy',
-        'pandas',
-        'matplotlib',
-        'obspy',
-        'geographiclib',
+    name="MagD",
+    version="2.0",
+    author="Jon Connolly",
+    author_email="joncon@uw.edu",
+    description="A map based packaged to analyze seimic network perfromance",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pnsn/magD",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-
 )
